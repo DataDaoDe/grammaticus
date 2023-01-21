dfcdc392c123c04a5c9cb9cd8e311cd7760bdbe0
John Faucett <jwaterfaucett@gmail.com>
2023-01-21 16:09:15 +0100

    Add to_singular, to_plural for first declension nouns

--- grammaticus/languages/latin/nouns.py
+++ grammaticus/languages/latin/nouns.py
@@ -45,5 +45,25 @@ def decline(stem: str, declension) -> pd.DataFrame:
     return forms
 
 
-def first_declension_to_singular(plural_noun: str):
+def first_declension_to_singular(plural_noun: str, case: str = None):
     stem, case_info = extract_first_declension_info(plural_noun)
+    if case:
+        return '{}{}'.format(stem, FIRST_DECLENSION[case]['singular'])
+    else:
+        plural_case_matches = list(
+            filter(lambda x: x[0] == 'plural', case_info))
+        if len(plural_case_matches) > 0:
+            first_match = plural_case_matches[0]
+            return '{}{}'.format(stem, FIRST_DECLENSION[first_match[1]]['singular'])
+
+
+def first_declension_to_plural(singular_noun: str, case: str = None):
+    stem, case_info = extract_first_declension_info(singular_noun)
+    if case:
+        return '{}{}'.format(stem, FIRST_DECLENSION[case]['plural'])
+    else:
+        singular_case_matches = list(
+            filter(lambda x: x[0] == 'singular', case_info))
+        if len(singular_case_matches) > 0:
+            first_match = singular_case_matches[0]
+            return '{}{}'.format(stem, FIRST_DECLENSION[first_match[1]]['plural'])
